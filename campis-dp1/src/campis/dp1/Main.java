6fce7b87449e41d794d0919024b41380633c1706
unknown <Administrador@fsandoval1.bdo.com.pe>
2017-10-25 12:12:22 -0500

    MainView added

--- campis-dp1/src/campis/dp1/Main.java
+++ campis-dp1/src/campis/dp1/Main.java
@@ -5,38 +5,37 @@
  */
 package campis.dp1;
 
+import java.io.IOException;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-
 /**
  *
  * @author Marco
  */
 public class Main extends Application {
-    
+    private Stage primaryStage;
+    private static BorderPane mainLayout;
+
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
+    public void start(Stage primaryStage) throws IOException {
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("CAMPIS");
+        showMainView();
+    }
+
+    private void showMainView() throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(Main.class.getResource("views/MainView.fxml"));
+        mainLayout = loader.load();
+        Scene scene = new Scene(mainLayout);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
