7fac0f810d44025317e85fc47326dfb5f88ab650
marcorm84 <a20111265@pucp.pe>
2017-10-29 14:11:13 -0500

    delete and search user working

--- campis-dp1/src/campis/dp1/controllers/users/ListController.java
+++ campis-dp1/src/campis/dp1/controllers/users/ListController.java
@@ -1,10 +1,13 @@
 package campis.dp1.controllers.users;
 
 import campis.dp1.Main;
+import campis.dp1.ContextFX;
 import campis.dp1.controllers.users.ListController;
 import campis.dp1.models.User;
 import campis.dp1.models.UserDisplay;
+import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
+import static java.lang.Boolean.TRUE;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.List;
@@ -13,8 +16,10 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import org.hibernate.Criteria;
@@ -31,7 +36,11 @@ public class ListController implements Initializable{
     private ObservableList<User> users;
     private ObservableList<UserDisplay> usersView;
     private int selected_id;
-
+    
+    @FXML
+    private Button searchButton;
+    @FXML
+    private JFXTextField searchField;
     @FXML
     private TableView<UserDisplay> tableUser;
     @FXML
@@ -52,9 +61,6 @@ public class ListController implements Initializable{
         main.showEditUser();
     }
 
-    /**
-     * Initializes the controller class.
-     */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         tableUser.getSelectionModel().selectedItemProperty().addListener(
@@ -98,11 +104,52 @@ public class ListController implements Initializable{
         usersView = FXCollections.observableArrayList();
         users = getUsers();
         for (int i = 0; i < users.size(); i++) {
-            UserDisplay user = new UserDisplay(users.get(i).getFirstname(), users.get(i).getLastname(), users.get(i).getEmail(), users.get(i).getUsername());
+            UserDisplay user = new UserDisplay(users.get(i).getId_user(), users.get(i).getFirstname(), users.get(i).getLastname(), users.get(i).getEmail(), users.get(i).getUsername());
             usersView.add(user);
         }
         tableUser.setItems(null);
         tableUser.setItems(usersView);
+    }
+
+    private ObservableList<User> getSearchList(String text) {
+        ObservableList<User> returnable;
+        returnable = FXCollections.observableArrayList();
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(User.class);
+        List<User> list = criteria.list();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getFirstname().contains(text) == TRUE || list.get(i).getLastname().contains(text) == TRUE) {
+                returnable.add(list.get(i));
+            }
+        }
+        sessionFactory.close();
+        return returnable;
+    }
+
+    @FXML
+    private void searchButtonAction(ActionEvent event) throws SQLException, ClassNotFoundException {
+        String text = this.searchField.getText();
+        if (text.compareTo("") == 0) {
+            loadData();
+        } else {
+            users = FXCollections.observableArrayList();
+            usersView = FXCollections.observableArrayList();
+            users = getSearchList(text);
+            if (users == null) {
+                tableUser.setItems(null);
+            } else {
+                for (int i = 0; i < users.size(); i++) {
+                    UserDisplay user = new UserDisplay(users.get(i).getId_user(), users.get(i).getFirstname(), users.get(i).getLastname(), users.get(i).getEmail(), users.get(i).getUsername());
+                    usersView.add(user);
+                }
+            }
+            tableUser.setItems(null);
+            tableUser.setItems(usersView);
+        }
     }
 
     private void deleteUser(int cod) {
@@ -125,8 +172,8 @@ public class ListController implements Initializable{
         ContextFX.getInstance().setId(selected_id);
         Integer id_user = ContextFX.getInstance().getId();
         deleteUser(selected_id);
-        for (int i = 0; i < useros.size(); i++) {
-            if (users.get(i).getId_user().compareTo(id_user) == 0) {
+        for (int i = 0; i < users.size(); i++) {
+            if(users.get(i).getId_user() == selected_id) {
                 users.remove(i);
             }
         }
