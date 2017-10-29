75c19ffc5d11ff3a8a83f533e577346d9d2ba105
gbustamante13 <g.bustamante1303@gmail.com>
2017-10-28 21:02:12 -0500

    list racks functionality added

--- campis-dp1/src/campis/dp1/controllers/racks/ListRackController.java
+++ campis-dp1/src/campis/dp1/controllers/racks/ListRackController.java
@@ -6,11 +6,27 @@
 package campis.dp1.controllers.racks;
 
 import campis.dp1.Main;
+import campis.dp1.controllers.products.ListController;
+import campis.dp1.models.Product;
+import campis.dp1.models.Rack;
+import campis.dp1.models.RackDisplay;
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 /**
  *
@@ -18,6 +34,8 @@ import javafx.fxml.Initializable;
  */
 public class ListRackController implements Initializable{
     private Main main;
+    private ObservableList<Rack> racks;
+    private ObservableList<RackDisplay> racksView;
     
     @FXML
     private void goNewRack() throws IOException {
@@ -29,11 +47,73 @@ public class ListRackController implements Initializable{
         main.showEditRack();
     }
 
+    @FXML
+    private TableView<RackDisplay> tablaRacks;
+    @FXML
+    private TableColumn<RackDisplay, Integer> idCol;
+    @FXML
+    private TableColumn<RackDisplay,  Integer> warehouseCol;
+    @FXML
+    private TableColumn<RackDisplay, Integer> numColumnsCol;
+    @FXML
+    private TableColumn<RackDisplay, Integer> numFloorsCol;
+    @FXML
+    private TableColumn<RackDisplay, Integer> orientationCol;
+    
+    private ObservableList<Rack> getRacks() {
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        Criteria criteria = session.createCriteria(Rack.class);
+        List lista = criteria.list();
+        ObservableList<Rack> returnable;
+        returnable = FXCollections.observableArrayList();
+        for (int i = 0; i < lista.size(); i++) {
+            returnable.add((Rack)lista.get(i));
+        }
+        sessionFactory.close();
+        return returnable;
+    }
+          
+    private void loadData() throws SQLException, ClassNotFoundException  {
+        racks = FXCollections.observableArrayList();
+        racksView = FXCollections.observableArrayList();
+        racks = getRacks();
+        
+        for (int i = 0; i < racks.size(); i++) {
+            System.out.println("campis.dp1.controllers.racks.ListRackController.loadData()");
+            System.out.println(racks.get(i).getId_rack());
+            
+            RackDisplay r = new RackDisplay(racks.get(i).getId_rack(), 
+                                            racks.get(i).getId_warehouse(),
+                                            racks.get(i).getN_columns(),
+                                            racks.get(i).getN_floors(),
+                                            racks.get(i).getOrientation());
+            racksView.add(r);
+        }
+        
+        tablaRacks.setItems(null);
+        tablaRacks.setItems(racksView);  
+    }
+          
      /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }  
+        try {
+            idCol.setCellValueFactory(cellData -> cellData.getValue().id_rackProperty().asObject());
+            warehouseCol.setCellValueFactory(cellData -> cellData.getValue().id_warehouseProperty().asObject());
+            numColumnsCol.setCellValueFactory(cellData -> cellData.getValue().n_columnsProperty().asObject());
+            numFloorsCol.setCellValueFactory(cellData -> cellData.getValue().n_floorsProperty().asObject());
+            orientationCol.setCellValueFactory(cellData -> cellData.getValue().orientationProperty().asObject());
+            
+            loadData();
+        } catch (SQLException | ClassNotFoundException ex) {
+            Logger.getLogger(ListController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    } 
+   
 }
---
+++ campis-dp1/src/campis/dp1/models/RackDisplay.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package campis.dp1.models;
+
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
+
+
+/**
+ *
+ * @author Gina Bustamante
+ */
+public class RackDisplay {
+    
+    private final IntegerProperty id_rack;
+    private final IntegerProperty id_warehouse;
+    //private final IntegerProperty pos_x;
+    //private final IntegerProperty pos_y;
+    private final IntegerProperty n_columns;
+    private final IntegerProperty n_floors;
+    private final IntegerProperty orientation;
+    
+    public RackDisplay(int id_rack, int id_warehouse, int n_columns, int n_floors, int orientation) {
+        this.id_rack = new SimpleIntegerProperty(id_rack);
+        this.id_warehouse = new SimpleIntegerProperty(id_warehouse);
+        //this.pos_x =  new SimpleIntegerProperty(pos_x);
+        //this.pos_y = new SimpleIntegerProperty(pos_y);
+        this.n_columns = new SimpleIntegerProperty(n_columns);
+        this.n_floors = new SimpleIntegerProperty(n_floors);
+        this.orientation = new SimpleIntegerProperty(orientation);
+    }
+    
+    public IntegerProperty id_rackProperty() {
+        return id_rack;
+    }
+    
+    public IntegerProperty id_warehouseProperty() {
+        return id_warehouse;
+    }
+    
+//    public IntegerProperty pos_xProperty() {
+//        return pos_x;
+//    }
+//    
+//    public IntegerProperty pos_yProperty() {
+//        return pos_y;
+//    }
+//    
+    public IntegerProperty n_columnsProperty() {
+        return n_columns;
+    }
+    
+    public IntegerProperty n_floorsProperty() {
+        return n_floors;
+    }
+    
+    public IntegerProperty orientationProperty() {
+        return orientation;
+    }
+}
