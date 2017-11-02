2030f18642e0173768c42c1fb1b11a72e1787d4f
bonf1re <bonfirelogs@gmail.com>
2017-11-02 01:57:59 -0500

    Add sidebar buttons and hibernate config

--- campis-dp1/src/campis/dp1/controllers/CommerceSidebarController.java
+++ campis-dp1/src/campis/dp1/controllers/CommerceSidebarController.java
@@ -22,4 +22,8 @@ public class CommerceSidebarController {
         main.showListRequestOrder();
     }
     
+    @FXML
+    private void goListRequestStatuses() throws IOException {
+        main.showListRequestStatuses();
+    }
 }
