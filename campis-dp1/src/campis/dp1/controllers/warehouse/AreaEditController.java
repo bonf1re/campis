fae951691d58a34bcec44d948d6a15db69e7bf16
Jose Amador <bonfirelogs@gmail.com>
2017-11-12 19:23:22 -0500

    Complete edit functionality

--- campis-dp1/src/campis/dp1/controllers/warehouse/AreaEditController.java
+++ campis-dp1/src/campis/dp1/controllers/warehouse/AreaEditController.java
@@ -8,10 +8,13 @@ package campis.dp1.controllers.warehouse;
 import campis.dp1.ContextFX;
 import campis.dp1.Main;
 import campis.dp1.models.Area;
+import campis.dp1.models.ProductType;
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
@@ -31,6 +34,7 @@ import org.hibernate.query.Query;
 public class AreaEditController implements Initializable{
     private int area_id;
     private Main main;
+    private ArrayList<ProductType> product_type_list= new ArrayList<>();
     
     @FXML
     private JFXTextField nameField;
@@ -42,6 +46,8 @@ public class AreaEditController implements Initializable{
     private JFXTextField posXField;
     @FXML
     private JFXTextField posYField;
+    @FXML
+    private JFXComboBox cbProductType;
 
     @FXML
     private void goListArea() throws IOException {
@@ -69,15 +75,44 @@ public class AreaEditController implements Initializable{
         SessionFactory sessionFactory = configuration.buildSessionFactory();
         Session session = sessionFactory.openSession();
         session.beginTransaction();
-        Query query = session.createQuery("update Area set name = :newName"
-                + " where id_role = :oldIdRole");
+        int id_pt = product_type_list.get(cbProductType.getSelectionModel().getSelectedIndex()).getId_prodType();
+        Query query = session.createQuery("update Area set name = :newName," + 
+                "length = :newLength," + "width = :newWidth," +
+                "pos_x = :newPosX," + "pos_y = :newPosY,"
+                + "product_type = :newProductType"
+                + " where id_area = :oldIdArea");
         query.setParameter("newName", nameField.getText());
-        query.setParameter("oldIdRole", area_id);
+        query.setParameter("newLength", Integer.parseInt(lengthField.getText()));
+        query.setParameter("newWidth", Integer.parseInt(widthField.getText()));
+        query.setParameter("newPosX", Integer.parseInt(posXField.getText()));
+        query.setParameter("newPosY", Integer.parseInt(posYField.getText()));
+        query.setParameter("newProductType", id_pt);
+        query.setParameter("oldIdArea", area_id);
         int result = query.executeUpdate();
         session.getTransaction().commit();
         session.close();
         sessionFactory.close();
         this.goListArea();
+    }
+    
+    private void loadProductTypeNames() {
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate.cfg.xml");
+        configuration.setProperty("hibernate.temp.use_jdbc_metadata_defaults","false");
+        SessionFactory sessionFactory = configuration.buildSessionFactory();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        
+        
+        Criteria criteria = session.createCriteria(ProductType.class);
+        this.product_type_list = (ArrayList<ProductType>) criteria.list();
+        ArrayList<String> pt_names = new ArrayList<>();
+        for (ProductType pt: this.product_type_list) {
+            pt_names.add(pt.getDescription());
+        }
+        session.close();
+        sessionFactory.close();
+        this.cbProductType.getItems().addAll(pt_names);
     }
 
     @Override
@@ -100,5 +135,7 @@ public class AreaEditController implements Initializable{
         this.posYField.setText(String.valueOf(result.getPos_y()));
         session.close();
         sessionFactory.close();
+        
+        loadProductTypeNames();
     } 
 }
