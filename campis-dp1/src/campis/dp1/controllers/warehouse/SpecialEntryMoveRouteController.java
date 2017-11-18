8bae0adb5935d9ad8b5632afeffe3e38686391f1
gbustamante13 <g.bustamante1303@gmail.com>
2017-11-18 14:55:16 -0500

    funciona cunaod la cantidad es menor que el maximo

--- campis-dp1/src/campis/dp1/controllers/warehouse/SpecialEntryMoveRouteController.java
+++ campis-dp1/src/campis/dp1/controllers/warehouse/SpecialEntryMoveRouteController.java
@@ -14,24 +14,14 @@ import campis.dp1.models.CNode;
 import campis.dp1.models.CRack;
 import campis.dp1.models.Coord;
 import campis.dp1.models.Coordinates;
-import campis.dp1.models.Product;
-import campis.dp1.models.Rack;
-import campis.dp1.models.TabuProblem;
-import campis.dp1.models.TabuSolution;
 import campis.dp1.models.Vehicle;
 import campis.dp1.models.utils.GraphicsUtils;
 import campis.dp1.models.Warehouse;
 import campis.dp1.models.WarehouseMove;
 import campis.dp1.models.WarehouseZone;
-import campis.dp1.models.routing.Grasp;
-import campis.dp1.models.routing.GraspResults;
-import campis.dp1.models.routing.RouteGen;
 import campis.dp1.models.utils.RoutingUtils;
-import campis.dp1.services.TabuSearchService;
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.net.URL;
-import java.sql.Date;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
@@ -50,12 +40,7 @@ import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
-import javafx.scene.control.cell.TextFieldTableCell;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Text;
-import javafx.util.StringConverter;
-import oracle.jrockit.jfr.tools.ConCatRepository;
 import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -112,19 +97,26 @@ public class SpecialEntryMoveRouteController implements Initializable{
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         this.id_warehouse=ContextFX.getInstance().getId();
-        this.routing_data=ContextFX.getInstance().getPolymorphic_list();
+        
+        this.routing_data = ContextFX.getInstance().getPolymorphic_list();
+        
         Vehicle vh =(Vehicle) ((ArrayList<Object>) this.routing_data.get((int)this.routing_data.get(0))).get(2);
+        /*
+        0 - indice
+        1 -
+*/
+        
         this.vhText.setText("Vehicle: "+ vh.getPlate() + " \nde capacidad : "+vh.getMax_weight());
         this.pCounterField.setText("<"+String.valueOf(this.routing_data.get(0))+"/"+String.valueOf(this.routing_data.size()-1)+">");
+        
         try{
             int m_test = ContextFX.getInstance().getWhMoveType() + 1;
             this.mode=1;
         }catch(Exception e){
             this.mode=0;
         }
+        
         try{
-            
-            
             qtCol.setCellValueFactory(cellData -> new SimpleIntegerProperty(cellData.getValue().getQuantity()).asObject());            
             prodCol.setCellValueFactory(cellData -> new SimpleStringProperty(this.product_names.get(this.batchesList.indexOf(cellData.getValue()))));
             zoneCol.setCellValueFactory(cellData -> cellData.getValue().getZoneStr());
@@ -135,8 +127,7 @@ public class SpecialEntryMoveRouteController implements Initializable{
             SessionFactory sessionFactory = configuration.buildSessionFactory();
             Session session = sessionFactory.openSession();
             session.beginTransaction();
-            
-            
+           
             
             loadData(session);
             drawMap(session);
@@ -201,7 +192,17 @@ public class SpecialEntryMoveRouteController implements Initializable{
         
         session.getTransaction().commit();
         session.close();
-        sessionFactory.close();        
+        sessionFactory.close();    
+        
+        this.goEntryMoveList();
+    }
+    
+    public void goWhEntryMoveList() throws IOException{
+        ContextFX.getInstance().setId(id_warehouse);
+        ContextFX.getInstance().setPolymorphic_list(null);
+        ContextFX.getInstance().setMode(0);
+        
+        main.showWhEntryMoveList();
     }
     
     @FXML
@@ -215,13 +216,17 @@ public class SpecialEntryMoveRouteController implements Initializable{
     public void nextRoute() throws IOException{
         int size = this.routing_data.size();
         int index = (int) this.routing_data.get(0);
+        
         if (index+1>size-1){
             System.out.println("No hay mas rutas");
             return;
         }
+        
         this.routing_data.set(0, index+1);
+        
         ContextFX.getInstance().setPolymorphic_list(routing_data);
         ContextFX.getInstance().setId(this.id_warehouse);
+        
         main.showWhEntryMoveRoute();
     }
     
